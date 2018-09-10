49d7cb02c86f30bad21693e11dcaae269676a5d7
Rodrigo de Lima Orofino <rodrigo.orofino@olxbr.com>
2018-09-10 02:22:34 -0300

    bump version

---
+++ __tests__/template/margin.expected.js
@@ -0,0 +1,9 @@
+import { View } from 'react-native';
+
+const LoadingSkeleton = BlaBla.extend`
+	margin-top: 1px;
+  margin: 0px auto;
+  margin: auto;
+`;
+
+export default LoadingSkeleton;
---
+++ __tests__/template/margin.tpl.js
@@ -0,0 +1,9 @@
+import { View } from 'react-native';
+
+const LoadingSkeleton = BlaBla.extend`
+	margin-top: 1;
+  margin: 0 auto;
+  margin: auto;
+`;
+
+export default LoadingSkeleton;
